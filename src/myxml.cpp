b624ec4f424ff70246e753959d05d8e2b481114f
tony <tony_barake@hotmail.com>
2024-08-06 21:35:45 -0400

    formatting

--- src/myxml.cpp
+++ src/myxml.cpp
@@ -1,91 +1,84 @@
 //////////////////////////////////////////////////////////////////////
-//myxml.cpp
-//implemetation of a simple xml "parser"
-//requires an input text stream (file)
-// does not interpret any XML escapes for < > or &..
-// Stream based.
+// myxml.cpp
+// implemetation of a simple xml "parser"
+// requires an input text stream (file)
+//  does not interpret any XML escapes for < > or &..
+//  Stream based.
 
 /*
 For Hosted Application Services PoC demo
 
 Rev		Date		Author		Comments
 ----------------------------------------
-0.1		Feb 23,2000	TB		
+0.1		Feb 23,2000	TB
 
-*/  
+*/
 
 #include "myxml.h"
 
 #include <ctype.h>
 #include <string.h>
 
-
-
 // Constructor/destructor
 // requires an input stream
 //
-MyXML::MyXML(FILE *inp, int m_bufsize): m_buf(0)
+MyXML::MyXML(FILE *inp, int m_bufsize) : m_buf(0)
 {
-m_inp=inp;
-m_inpType = FILEBUF;
+	m_inp = inp;
+	m_inpType = FILEBUF;
 
-//m_stack = new Stack;
-m_buf = new char[m_bufsize];
-ResetParser();
+	// m_stack = new Stack;
+	m_buf = new char[m_bufsize];
+	ResetParser();
 }
 
-
-//read input from a buffer
-MyXML::MyXML(char *buf, int m_bufsize): m_buf(0)
+// read input from a buffer
+MyXML::MyXML(char *buf, int m_bufsize) : m_buf(0)
 {
-m_cinp=buf;
-m_inpType = CHARBUF;
-m_inpBuf = buf;
+	m_cinp = buf;
+	m_inpType = CHARBUF;
+	m_inpBuf = buf;
 
-//m_stack = new Stack;
-m_buf = new char[m_bufsize];
-ResetParser();
+	// m_stack = new Stack;
+	m_buf = new char[m_bufsize];
+	ResetParser();
 }
-
 
 MyXML::~MyXML()
 {
-//TODO: destroy stack..
-if (m_buf)
-        delete[] m_buf;
+	// TODO: destroy stack..
+	if (m_buf)
+		delete[] m_buf;
 }
 
-
-void 
-MyXML::ResetParser(){
+void MyXML::ResetParser()
+{
 	m_pbuf = m_buf;
-	m_state=START;
+	m_state = START;
 	m_nesting = 0;
 }
 
-
-//go back to the start of the stream
+// go back to the start of the stream
 //
-void 
-MyXML::Rewind() {
+void MyXML::Rewind()
+{
 	ResetParser();
-	switch (m_inpType) {
+	switch (m_inpType)
+	{
 	case CHARBUF:
 		m_cinp = m_inpBuf;
 		break;
 	case FILEBUF:
-		fseek(m_inp,0L,SEEK_SET);
+		fseek(m_inp, 0L, SEEK_SET);
 		break;
-	default:
-		;
+	default:;
 	}
 }
 
-
-
-int
-MyXML::NextChar() {
-	switch (m_inpType) {
+int MyXML::NextChar()
+{
+	switch (m_inpType)
+	{
 	case CHARBUF:
 		if (!m_cinp)
 			return EOF;
@@ -96,119 +89,124 @@ MyXML::NextChar() {
 	case FILEBUF:
 		if (!feof(m_inp))
 			return fgetc(m_inp);
-	default:
-		;
+	default:;
 	}
 	return EOF;
 }
 
-
-
-
-
 /////////////////////////////////////
 // take up the parsing where left off
-// returns the next token, and state 
-// m_buf, and m_state 
+// returns the next token, and state
+// m_buf, and m_state
 // or NULL if EOF
 //
 
 char *
 MyXML::Parse()
 {
-int c;
-m_pbuf = m_buf;		//reset buffer
+	int c;
+	m_pbuf = m_buf; // reset buffer
 
-while (EOF != (c = NextChar() ) ) {
-        switch (m_state) {
+	while (EOF != (c = NextChar()))
+	{
+		switch (m_state)
+		{
 
-        case START:
-                if ( c == '<')
-                        m_state = BEGIN_TAG;
-                break;
+		case START:
+			if (c == '<')
+				m_state = BEGIN_TAG;
+			break;
 
-        case BEGIN_TAG:
-                if ( isspace(c) ) break;
-                if ( c == '/' ) {
-                        m_state = TERM_TAG;
-                }
-                else if ( c == '>' ) {
-                        m_state = END_TAG;        //empty tag...
-                }
-                else {
-                        m_state = TAG;            //begin storing the tag...
-                        *m_pbuf++ = (char) c;
-                }
-                break;
+		case BEGIN_TAG:
+			if (isspace(c))
+				break;
+			if (c == '/')
+			{
+				m_state = TERM_TAG;
+			}
+			else if (c == '>')
+			{
+				m_state = END_TAG; // empty tag...
+			}
+			else
+			{
+				m_state = TAG; // begin storing the tag...
+				*m_pbuf++ = (char)c;
+			}
+			break;
 
-        case TAG:
-                if ( isspace(c) )  break;
-                if ( c == '>' ) {
-                        m_state = END_TAG;
-                        // m_stack.push ( m_buf ); //for validation...
-						m_nesting++;
-						*m_pbuf = NULL;
-						//////////////////////
-                        return m_buf;              //tag value m_state = END_TAG
-                }
-                else {
-                        *m_pbuf++ = (char) c;      //save the character
-                }
-                break;
+		case TAG:
+			if (isspace(c))
+				break;
+			if (c == '>')
+			{
+				m_state = END_TAG;
+				// m_stack.push ( m_buf ); //for validation...
+				m_nesting++;
+				*m_pbuf = NULL;
+				//////////////////////
+				return m_buf; // tag value m_state = END_TAG
+			}
+			else
+			{
+				*m_pbuf++ = (char)c; // save the character
+			}
+			break;
 
-        case END_TAG:
-                if (c == '<' ) {
-                        m_state = BEGIN_TAG;
+		case END_TAG:
+			if (c == '<')
+			{
+				m_state = BEGIN_TAG;
 
-						*m_pbuf = NULL;
-						//////////////////////
-                        return m_buf;              //BEGIN_TAG means we have a field!
-                }
-                else {
-                        *m_pbuf++ = (char) c;   //save field character incl spaces
-                }
-                break;
+				*m_pbuf = NULL;
+				//////////////////////
+				return m_buf; // BEGIN_TAG means we have a field!
+			}
+			else
+			{
+				*m_pbuf++ = (char)c; // save field character incl spaces
+			}
+			break;
 
+		case TERM_TAG:
+			if (isspace(c))
+				break; // ignore spaces in tag
+			if (c == '>')
+			{
+				m_state = END_TERM_TAG;
+				m_nesting--;
+				// TODO: do a compare to the stack
+				*m_pbuf = NULL;
+				//////////////////////
+				return m_buf; // return the END_TERM_TAG without the slash...
+			}
+			else
+			{
+				*m_pbuf++ = (char)c; // save tag char to buffer
+			}
+			break;
 
-        case TERM_TAG:
-                if (isspace(c)) break;          //ignore spaces in tag
-                if (c == '>' ) {
-                        m_state = END_TERM_TAG;
-						m_nesting--;
-                        //TODO: do a compare to the stack
-						*m_pbuf = NULL;
-						//////////////////////
-                        return m_buf;              //return the END_TERM_TAG without the slash...
-                }
-                else {
-                        *m_pbuf++ = (char) c;   //save tag char to buffer
-                }
-                break;
+		case END_TERM_TAG:
+			if (c == '<')
+			{ // ignore everthing except <
+				m_state = BEGIN_TAG;
+			}
+			break;
 
-
-        case END_TERM_TAG:
-                if (c == '<' ) {                        //ignore everthing except <
-                        m_state = BEGIN_TAG;
-                }
-                break;
-
-        default:
-				;
-        }//switch
-}//while
-m_state = EOFSTATE;
-return NULL;
+		default:;
+		} // switch
+	} // while
+	m_state = EOFSTATE;
+	return NULL;
 }
 
-
-int 
-MyXML::GetNesting() {
+int MyXML::GetNesting()
+{
 	return m_nesting;
 }
 
-
 ////////////////////////////
-// return the next tag - 
+// return the next tag -
 // starting tags only..<TAG>
 // ignore terminating tags </TAG>
 // white space is ignored
@@ -217,13 +215,13 @@ MyXML::GetNextTag()
 {
 	char *result;
 
-	do {
+	do
+	{
 		result = Parse();
 	} while (m_state != END_TAG && result != NULL);
 
 	return result;
 }
-
 
 /////////////////////////////////////////
 // return the next data field (not a tag)
@@ -233,7 +231,8 @@ MyXML::GetNextDataField()
 {
 	char *result;
 
-	do {
+	do
+	{
 		result = Parse();
 	} while (m_state != BEGIN_TAG && result != NULL);
 
@@ -241,44 +240,43 @@ MyXML::GetNextDataField()
 }
 
 ////////////////////////////
-// return the next field that matches 
+// return the next field that matches
 //	the tag parameter from the stream
 //	or NULL if not found
 //
 char *
-MyXML::GetNextTaggedField(char* tag)
+MyXML::GetNextTaggedField(char *tag)
 {
 	char *result;
 
 	// find the tag we need
 	result = GetNextTag(tag);
 
-	//fetch the field
+	// fetch the field
 	if (m_state == END_TAG && result != NULL)
 		result = GetNextDataField();
 
 	return result;
 }
 
-
 ////////////////////////////
-// return the next tag that matches 
+// return the next tag that matches
 //	the tag parameter
 //	or NULL if not found
 // used to position the stream pointer
 //
 char *
-MyXML::GetNextTag(char* tag)
+MyXML::GetNextTag(char *tag)
 {
 	char *result;
-	if (!m_buf || (*m_buf==0))
+	if (!m_buf || (*m_buf == 0))
 		return NULL;
 
-	while (strcmp(m_buf, tag) != 0) {
-		result = GetNextTag();	
+	while (strcmp(m_buf, tag) != 0)
+	{
+		result = GetNextTag();
 		if (result == NULL)
 			break;
-		} 
+	}
 	return result;
 }
-
--- src/myxml.h
+++ src/myxml.h
@@ -7,7 +7,7 @@
 
 // MyXML.h
 // A simple state machine based non-validating XML "parser"
-// optional: may later use string and stack class - for now just counts 
+// optional: may later use string and stack class - for now just counts
 // nesting levels of tags
 //////////////////////////////////////////////////////////////////////
 //
@@ -16,47 +16,57 @@ For Hosted Application Services PoC demo
 
 Rev		Date		Author		Comments
 ----------------------------------------
-0.1		Feb 23,2000	TB		
-*/  
-
+0.1		Feb 23,2000	TB
+*/
 
 #include <stdio.h>
 
-
-
-class MyXML {
+class MyXML
+{
 public:
-        MyXML(FILE* inp, int m_bufsize=4096);                //requires an input stream
-		MyXML(char *buf, int m_bufsize=4096);				 //zero terminated buffer not UNICODE..
-        ~MyXML();
+	MyXML(FILE *inp, int m_bufsize = 4096); // requires an input stream
+	MyXML(char *buf, int m_bufsize = 4096); // zero terminated buffer not UNICODE..
+	~MyXML();
 
 public:
-		const char* GetCurrentBuf() {return m_buf;};	//whatever was fetched last...
-        char* GetNextTag();             //next tag in stream or null
-        char* GetNextTag(char* tag);    //next matching tag in stream or null
-        char* GetNextDataField();	    //next data field in stream or null
-        char* GetNextTaggedField(char* tag); //search for first occurence of tag, get data or null
-		int	  GetNesting();				//get current nesting level for what it's worth
-		void  Rewind();					//rewind the buffer to the start to re-parse
+	const char *GetCurrentBuf() { return m_buf; }; // whatever was fetched last...
+	char *GetNextTag();							   // next tag in stream or null
+	char *GetNextTag(char *tag);				   // next matching tag in stream or null
+	char *GetNextDataField();					   // next data field in stream or null
+	char *GetNextTaggedField(char *tag);		   // search for first occurence of tag, get data or null
+	int GetNesting();							   // get current nesting level for what it's worth
+	void Rewind();								   // rewind the buffer to the start to re-parse
 protected:
-		int NextChar();
-		void ResetParser();
-		char *Parse();
-		FILE *m_inp;
-		char *m_cinp;
-		char *m_inpBuf;
-        enum State {START, BEGIN_TAG, TERM_TAG, TAG, END_TERM_TAG, END_TAG, EOFSTATE};
-        State m_state;
-        char *m_buf;
-        char *m_pbuf;
-		int	  m_nesting;
+	int NextChar();
+	void ResetParser();
+	char *Parse();
+	FILE *m_inp;
+	char *m_cinp;
+	char *m_inpBuf;
+	enum State
+	{
+		START,
+		BEGIN_TAG,
+		TERM_TAG,
+		TAG,
+		END_TERM_TAG,
+		END_TAG,
+		EOFSTATE
+	};
+	State m_state;
+	char *m_buf;
+	char *m_pbuf;
+	int m_nesting;
+
 private:
-	enum InpType { CHARBUF, FILEBUF};
+	enum InpType
+	{
+		CHARBUF,
+		FILEBUF
+	};
 	InpType m_inpType;
 	int m_bufsize;
 
-}; //MyXML
-
+}; // MyXML
 
 #endif
-
